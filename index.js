0d04d0213bd796305ad3908d9156c0c07486ec38
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-13 14:13:19 +0530

    id founder done

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 
+
 const PORT = 3000;
 
-app.get('/item', (req, res) => {
-    console.log("Hey");
-    res.send("Hey")
+let items = [
+    {id: 1, name: "Pen"},
+    {id: 2, name: "Book"},
+];
+
+app.use(express.json());
+
+app.get('/items', (req, res) => {
+    res.status(200).json(items);
+});
+
+app.get('/items:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    const item = items.find( i => i.id === id);
+    if(!item) {
+        return res.status(404).json({error: "Item not found"});
+    }
+
 } )
 
 app.listen(PORT, () => {
